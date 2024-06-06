d92785c0e16480b7693b7d02956457693ba21ad8
KavtaradzeN <ninikavtaradze77@gmail.com>
2024-06-07 00:46:51 +0400

    add grocery exsercise

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,1 +1,13 @@
 emptydict = {}
+
+while True:
+    try:
+        grocery = input().upper()
+        if grocery not in emptydict:
+           emptydict[grocery]= 1
+        else:
+            emptydict[grocery] += 1
+    except  EOFError:
+        for key,value in sorted(emptydict.items()):
+            print(value,key)
+        break
